cb42085bc43994d726fe0d4a926883cc70db8e22
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-30 10:07:21 -0600

    brought in firebase auth properties to use with signing in and signing out

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import authReducer from './authReducer'
 import placeReducer from './placeReducer'
 import { combineReducers } from 'redux'
 import { firestoreReducer } from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 export  const rootReducer = combineReducers({
   auth: authReducer,
   place: placeReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 })
