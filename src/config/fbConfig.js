84e3e7e356a88213f1a2dcd5fbc9d0b21c7d1436
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-29 19:53:39 -0600

    Brought in firebase librarys and brought in config to connect it to firebase features

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBeqfhdWjQ6pqc7De4FLSTyplzcLq7H9iw",
+  authDomain: "tinderfood-83ba7.firebaseapp.com",
+  databaseURL: "https://tinderfood-83ba7.firebaseio.com",
+  projectId: "tinderfood-83ba7",
+  storageBucket: "tinderfood-83ba7.appspot.com",
+  messagingSenderId: "1013681643309",
+  appId: "1:1013681643309:web:022b31184b50a99e85bda4",
+  measurementId: "G-4JPNB788E0"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase;
