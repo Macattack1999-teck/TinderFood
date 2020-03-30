2f8dc511388c2d9a344f747c665c7c18a153b77c
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-30 16:00:13 -0600

    Set up user Sign Up functionality

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
+import { signUp } from '../../store/actions/authActions'
+
 class SignUp extends Component {
   state = {
     email: '',
@@ -17,11 +19,11 @@ class SignUp extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    console.log(this.state)
+    this.props.signUp(this.state)
   }
 
   render() {
-    const { auth } = this.props
+    const { auth, authError } = this.props
     if (auth.uid) return <Redirect to="/" />
     return (
       <div className="container">
@@ -49,6 +51,9 @@ class SignUp extends Component {
           </div>
           <div className="button-wrapper">
             <button className="btn">Sign Up!</button>
+            <div className="error">
+              { authError ? <p>{ authError }</p> : null }
+            </div>
           </div>
         </form>
       </div>
@@ -58,8 +63,15 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
-export default connect(mapStateToProps)(SignUp)
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
