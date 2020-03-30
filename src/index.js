815ff1e83763eaae9389c5580696524cdff93d74
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-30 11:14:06 -0600

    Waiting for auth is ready to render page

--- src/index.js
+++ src/index.js
@@ -8,40 +8,61 @@ import { rootReducer } from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import {
-  reduxFirestore,
+  createFirestoreInstance,
   getFirestore,
-  createFirestoreInstance
+  reduxFirestore
 } from "redux-firestore";
 import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
 import fbConfig from "./config/fbConfig";
 import firebase from "firebase/app";
+
+//for render on auth ready
+import { useSelector  } from 'react-redux'
+import { isLoaded  } from 'react-redux-firebase';
+
+
 
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-    reduxFirestore(fbConfig)
+    reduxFirestore(firebase, fbConfig)
   )
 );
+
+const profileSpecificProps = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+  enableRedirectHandling: false,
+  resetBeforeLogin: false
+}
 
 
 const rrfProps = {
   firebase,
   config: fbConfig,
+  config: profileSpecificProps,
   dispatch: store.dispatch,
   createFirestoreInstance
 };
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) return <div className="center"> <p>Loading...</p></div>;
+      return children
+}
 
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded>
       <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
-
 
 serviceWorker.unregister();
