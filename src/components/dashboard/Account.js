f8134f9504479f157d4e0b0e8b6f27afa1cafbc9
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-30 21:20:26 -0600

    Outputting user account info in the account component

--- src/components/dashboard/Account.js
+++ src/components/dashboard/Account.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
-const Account = () => {
-  return (
-    <div className="container">
-      <span>Email</span>
-      <span>Username</span>
-      <span>Admin</span>
-    </div>
-  )
+class Dashboard extends Component {
+  render() {
+    const { auth, profile } = this.props;
+    if (!auth.uid) return <Redirect to='/signin' />
+
+    return (
+      <div className="container">
+          <div>Email: {auth.email}</div>
+          <div>Username: {profile.userName}</div>
+      </div>
+    )
+  }
 }
 
-export default Account
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard)
